13603f92b3c3549dab19edbea67c32fe8d3acd45
michael <michaelngige@gmail.com>
2023-09-21 10:23:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 8;
+let num2 = 7.75;
+let multiply = num1 * num2;
+
+var random = Math.round(0.5);
+
+const num3 = 20;
+const num4 = 8;
+let mod = num3 % num4;
+
+ 
+let max = Math.max(16, 17, 9, 10, 20);
+
